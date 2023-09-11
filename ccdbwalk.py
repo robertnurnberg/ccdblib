1d56223f1aaadf36fb3363f44a11fb6bbb359153
Robert Nurnberg @ elitebook <robert.nurnberg@gmx.de>
2023-09-11 18:00:43 +0200

    ccdbwalk

---
+++ ccdbwalk.py
@@ -0,0 +1,245 @@
+"""
+   Script that makes Chinese chessdb.cn explore certain openings or book exits.
+"""
+import argparse, asyncio, math, random, time, sys, os, ccdblib
+
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "python-chinese-chess"))
+import cchess
+
+
+def select_move(movelist, temp):
+    # select a random move from movelist, using an exponential distribution in the argument (score - bestscore) / temp
+    if temp <= 0:  # return best move if temp <= 0
+        return movelist[0]["uci"]
+    weights = []
+    first, score = True, 0
+    for m in movelist:
+        if (
+            "score" in m and type(m["score"]) == int
+        ):  # if e.g. m["score"] == "??", then use score from previous move
+            score = m["score"]
+        if first:
+            first, best = False, score
+        weights.append(math.exp((score - best) / temp))
+    p = random.random() * sum(weights)
+    wsum = 0
+    for i, m in enumerate(movelist):
+        if (wsum := wsum + weights[i]) > p:
+            break
+    return m["uci"]
+
+
+class ccdbwalk:
+    def __init__(
+        self,
+        filename,
+        verbose,
+        moveTemp,
+        backtrack,
+        depthLimit,
+        TBwalk,
+        concurrency,
+        user,
+    ):
+        self.filename = filename
+        self.isPGN = filename.endswith(".pgn")
+        self.verbose = verbose
+        self.moveTemp = moveTemp
+        self.backtrack = backtrack
+        self.depthLimit = depthLimit
+        self.TBwalk = TBwalk
+        self.concurrency = concurrency
+        self.ccdb = ccdblib.ccdbAPI(concurrency, user)
+
+    def reload(self):
+        self.metalist = []
+        if self.isPGN:
+            # pgn support in cchess seems to be rudimentary for now:
+            # just one game per .pgn and we only get the final board
+            board = cchess.Board.from_pgn(self.filename)
+            self.metalist.append(board)
+            print(
+                f"Read {len(self.metalist)} (opening) lines from file {self.filename}."
+            )
+        else:
+            with open(self.filename) as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        if line.startswith("#"):  # ignore comments
+                            continue
+                        fen = line.split()[:6]  # include potential move counters
+                        if len(fen) == 6 and not (
+                            fen[4].isnumeric() and fen[5].isnumeric()
+                        ):
+                            fen = fen[:4]
+                        fen = " ".join(fen)
+                        self.metalist.append(fen)
+            print(f"Read {len(self.metalist)} FENs from file {self.filename}.")
+        self.gn = len(self.metalist)
+
+    async def parse_all(self, batchSize=None):
+        print(
+            f"Started parsing the positions with concurrency {self.concurrency}"
+            + (" ..." if batchSize == None else f" and batch size {batchSize} ..."),
+        )
+        if batchSize is None:
+            batchSize = len(self.metalist)
+        self.tic = time.time()
+        for i in range(0, len(self.metalist), batchSize):
+            tasks = []
+            for lineIdx in range(i, min(i + batchSize, len(self.metalist))):
+                tasks.append(asyncio.create_task(self.parse_single_line(lineIdx)))
+
+            for parse_line in tasks:
+                p = await parse_line
+                if p:
+                    print(p)
+
+        elapsed = time.time() - self.tic
+        print(
+            f"Done processing {self.filename} in {elapsed:.1f}s.",
+        )
+
+    async def parse_single_line(self, lineIdx):
+        line = self.metalist[lineIdx]
+        if self.isPGN:
+            board = line
+        else:
+            board = cchess.Board(line)
+        r = await self.ccdb.showall(board.epd())
+        score = ccdblib.json2eval(r)
+        retStr = ""
+        if self.verbose:
+            retStr += f"FEN {lineIdx+1}/{self.gn}: {board.epd()} ("
+            retStr += f"{score}{'cp' if type(score) is int else ''}) "
+            url = f"https://chessdb.cn/query_en/?{board.epd()}"
+        if board.turn == cchess.RED:
+            ply0 = 0
+        else:
+            ply0 = 1
+            if self.verbose:
+                retStr += f"1... "
+        ply = ply0
+        while "moves" in r and ply - ply0 < self.depthLimit:
+            m = select_move(r["moves"], temp=self.moveTemp)
+            move = cchess.Move.from_uci(m)
+            if self.verbose:
+                retStr += f"{m} "
+                if ply == ply0:
+                    url += " moves"
+                url += " " + m
+            board.push(move)
+            ply += 1
+            if o := board.outcome() is not None:
+                r = {}
+                if self.verbose:
+                    retStr += o.result()
+            elif not self.TBwalk and (
+                (pc := sum(p in "rnckabp" for p in board.epd().lower().split()[0])) <= 9
+                or (apc := sum(p in "rnc" for p in board.epd().lower().split()[0])) <= 3
+            ):
+                r = {}
+                if self.verbose:
+                    retStr += f"[{pc}pieces w/ {apc}attackers limit]"
+            else:
+                r = await self.ccdb.queryall(board.epd())
+        if self.verbose >= 3:
+            retStr += f'\n  URL: {url.replace(" ", "_")}'
+        bt = 0
+        while bt <= self.backtrack:
+            asyncio.ensure_future(self.ccdb.queue(board.epd()))
+            bt += 1
+            if not board.move_stack:
+                break
+            board.pop()
+        if bt and self.verbose >= 2:
+            retStr += f"\n  Plies queued for analysis: {ply} ... {ply-bt+1}."
+        return retStr
+
+
+async def main():
+    parser = argparse.ArgumentParser(
+        description="A script that walks within the Chinese chessdb.cn tree, starting from FENs or lines in a PGN file. Based on the given parameters, the script selects a move in each node, walking towards the leafs. Once an unknown position is reached, it is queued for analysis and the walk terminates.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "filename", help="PGN file if suffix is .pgn, o/w a text file with FENs"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=0,
+        help="Increase output with -v, -vv, -vvv etc.",
+    )
+    parser.add_argument(
+        "--moveTemp",
+        type=float,
+        default=10,
+        help="Temperature T for move selection: in each node of the tree the probability to pick a move m will be proportional to exp((score(m)-score(bestMove))/T). Here unscored moves get assigned the score of the currently worst move. If T is zero, then always select the best move.",
+    )
+    parser.add_argument(
+        "--backtrack",
+        type=int,
+        default=0,
+        help="The number of plies to walk back from the newly created leaf towards the root, queuing each position on the way for analysis.",
+    )
+    parser.add_argument(
+        "--depthLimit",
+        help="The upper limit of plies the walk is allowed to last.",
+        type=int,
+        default=200,
+    )
+    parser.add_argument(
+        "--TBwalk",
+        action="store_true",
+        help="Continue the walk beyond piece limit (>= 10 pieces and >= 4 attackers.",
+    )
+    parser.add_argument(
+        "-c",
+        "--concurrency",
+        help="Maximum concurrency of requests to ccdb.",
+        type=int,
+        default=16,
+    )
+    parser.add_argument(
+        "-b",
+        "--batchSize",
+        help="Number of positions processed in parallel. Small values guarantee more responsive output, large values give faster turnaround.",
+        type=int,
+        default=None,
+    )
+    parser.add_argument(
+        "-u",
+        "--user",
+        help="Add this username to the http user-agent header",
+    )
+    parser.add_argument(
+        "--forever",
+        action="store_true",
+        help="Run the script in an infinite loop.",
+    )
+    args = parser.parse_args()
+
+    walk = ccdbwalk(
+        args.filename,
+        args.verbose,
+        args.moveTemp,
+        args.backtrack,
+        args.depthLimit,
+        args.TBwalk,
+        args.concurrency,
+        args.user,
+    )
+    while True:  # if args.forever is true, run indefinitely; o/w stop after one run
+        # re-reading the data in each loop allows updates to it in the background
+        walk.reload()
+        await walk.parse_all(args.batchSize)
+
+        if not args.forever:
+            break
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
